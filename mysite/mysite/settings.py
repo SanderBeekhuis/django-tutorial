438c5561158a5715207e31c5f965e30da72696bf
Sander <Sander.Beekhuis@ordina.nl>
2021-01-15 12:12:43 +0100

    Colorize unit tests

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Colorize unit tests
+TEST_RUNNER="redgreenunittest.django.runner.RedGreenDiscoverRunner"
