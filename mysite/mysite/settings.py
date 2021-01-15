564e58525ef30691d91e78e371c413315b477f71
Sander <Sander.Beekhuis@ordina.nl>
2021-01-15 14:30:56 +0100

    Improve admin site

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
-    'django.contrib.admin',
+    'mysite.apps.MyAdminConfig',  # replaces 'django.contrib.admin'
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
