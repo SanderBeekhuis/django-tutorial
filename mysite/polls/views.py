40b7dbd3e6528a55c83de5c8bbcc7246807061af
Sander <Sander.Beekhuis@ordina.nl>
2021-01-15 12:45:10 +0100

    Add even more tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,6 @@
-from datetime import timezone
-from django.http.response import Http404, HttpResponseRedirect
+from django.utils import timezone
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from django.template import loader
 from django.urls import reverse
 from django.views import generic
 
@@ -29,6 +27,12 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
